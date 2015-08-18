bef56e98dd1a521ff94a54e644a93c3a8b0c5225
lewis <lewis.dawson@acano.com>
2015-08-18 11:45:07 +0000

    print the address of received connections

--- scripts/server.c
+++ scripts/server.c
@@ -7,27 +7,48 @@
 #include <netinet/in.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <signal.h>
 
-#define PORT "7778" //the port users will be connecting to
+#define PORT "7777" //the port users will be connecting to
 #define BACKLOG 20
 #define BUF_SIZE 256 
 
-int main() {
+int status, socket_fd, new_fd, bind_r;
+struct addrinfo hints, *info, *p;
+struct sockaddr_storage conn_addr;
+socklen_t addr_size;
+char recv_buf[BUF_SIZE];
+char *msg = "Message from the server!\n";
+char addr[INET_ADDRSTRLEN];
 
-	int status, socket_fd, new_fd, bind_r;
-	struct addrinfo hints, *info, *p;
-	struct sockaddr_storage conn_addr; //connector's address info
-	socklen_t addr_size;
-	char recv_buf[BUF_SIZE];
-	char *msg = "Message from the server!\n";
-	
+//cast sockaddr to sockaddr_in
+
+void * convert(struct sockaddr * sa) {
+	struct sockaddr_in *tmp;
+	//do the cast	
+	tmp = (struct sockaddr_in *)sa;
+	return (&(tmp->sin_addr));
+	}
+
+void sig_handler(int signal) {
+	if (signal == SIGINT) {
+		printf("SIGINT signal received - exiting now\n");
+		close(socket_fd);
+		exit(1);
+	}
+
+}
+
+int main() {
+	//set up the signal handler
+	signal(SIGINT, sig_handler);
 
 	memset(&hints, 0, sizeof(hints));
 
 	hints.ai_family = AF_INET;
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_flags = AI_PASSIVE;
-	
+
 	if ((status = getaddrinfo(NULL, PORT, &hints, &info)) < 0) {
 		fprintf(stderr, "failed to get address information");
 		exit(1);
@@ -47,7 +68,7 @@ int main() {
 		}
 		break;
 	} 	
-	
+
 	if (p == NULL) {
 		fprintf(stderr, "Failed to bind\n");
 		exit(1);
@@ -57,22 +78,24 @@ int main() {
 		perror("listen");
 		exit(1);
 	}
-	
+
 	printf("Server: waiting for connections\n");
 
 	while(1) {
 		addr_size = sizeof(conn_addr);		
-		new_fd = accept(socket_fd,(struct sockaddr *)&conn_addr, &addr_size);
+		new_fd = accept(socket_fd, (struct sockaddr *)&conn_addr, &addr_size);
 		if (new_fd < 1) {
 			perror("server: accept");
 			continue;
 		}
-		if (send(new_fd, msg, sizeof(msg), 0) < 0) {
+		//print IP of who has connected to the server
+		inet_ntop(AF_INET, convert((struct sockaddr *)&conn_addr), addr, sizeof(addr));	
+		fprintf(stdout, "Received a connection from: %s\n", addr);
+		if (send(new_fd, "test message\n", 13, 0) < 0) {
 			perror("send");
 		}
 		close(new_fd);
-		close(socket_fd);
-
-	return (0);
 	}
+	close(socket_fd);
+	return (0);
 }
