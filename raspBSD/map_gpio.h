d9c5ee675bb2e232c7d99cb6138fbffae0e5dae4
Lewis Dawson <lewis.dawson@acano.com>
2015-08-21 16:35:08 +0100

    Tidy up a little bit; make map_gpio a header file

--- raspBSD/map_gpio.h
+++ raspBSD/map_gpio.h
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/mman.h>
 
 #define PAGE_SIZE 4096
 #define PERI_BASE_ADDR 0x20000000
@@ -12,14 +16,10 @@ struct peripheral {
 	int map_fd; //file descriptor when opening /dev/mem
 	void *map; //pointer to map returned by mmap
 	unsigned long p_addr; //physical address we want to map = offset in the /dev/mem mmap
-}
+	volatile unsigned int *cast_map; //cast map as pointer to volatile unsigned int
+};
 
-struct peripheral *p
-
-void map_gpio(unsigned long gpio_addr) {
-
-	//pointer to peripheral structure
-	p->p_addr = gpio_addr;
+void map_gpio(struct peripheral *p) {
 
 	//open /dev/mem
 	if (p->map_fd = open("/dev/mem", O_RDWR) < 0) {
@@ -30,28 +30,33 @@ void map_gpio(unsigned long gpio_addr) {
 	//close file descriptor
 	close(p->map_fd);
 	//create a mapping to gpio's physical address p_addr
-	if (p->map = mmap(0, PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED,  p->map_fd, p->p_addr)< 0) {
+	p->map = mmap(0, PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED,  p->map_fd, p->p_addr); 
+	if (p->map == MAP_FAILED) {
 		perror("mmap");
 		exit(1);
 	}
+
+	p->cast_map = (volatile unsigned int *)p->map;
 }
 
-void unmap_gpio(void) {
-	munmap(p->map);
+void unmap_gpio(struct peripheral *p) {
+	munmap(p->map, PAGE_SIZE);
 }
 
 //set a GPIO pin as input
-void set_input(int pin) {
-	unsigned int addr, bit;
-	addr = p->p_map + (pin/10); //each function select register allows you to set 10 pins
+void set_input(struct peripheral *p, int pin) {
+	unsigned int bit;
+	volatile unsigned int *addr;
+	addr = p->cast_map + (pin/10); //each function select register allows you to set 10 pins
 	//input requires appropriate bits to be set as 000, so bitshift 111 to correct bit position and then take inverse 
 	bit = ~(7 << (pin % 10)*3);
 	*addr &= bit; //keep old registers values the same
 }
 
-void set_output(int pin) {
-	unsigned int addr, bit;
-	addr = p->p_map + (pin/10);
+void set_output(struct peripheral *p, int pin) {
+	unsigned int bit;
+	volatile unsigned int *addr;
+	addr = p->cast_map + (pin/10);
 	//output required appropriate bits to be set as 001, so bit shift decimal 1 to correct bit position
 	bit = 1 << (pin % 10)*3;
 	*addr |= bit; 
