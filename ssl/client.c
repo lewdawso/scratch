604f8de7f559146b473e5784dedbae527c910b4c
Lewis Dawson <lewis.dawson@acano.com>
2016-05-07 21:34:29 +0100

    Initial commit - simple network connection program for ssl

---
+++ ssl/client.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <string.h>
+#include <netdb.h>
+
+static int sockfd;
+static struct addrinfo hints, *res; 
+
+const char *addr = "www.google.co.uk";
+const char *port = "443";
+
+int main (int argc, char *argv[]) {
+
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+
+    if ((getaddrinfo(addr, port, &hints, &res)) != 0) {
+        perror("getaddrinfo");
+        return 1;
+    }
+    
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1 ) {
+        perror("socket");
+        return 1;
+    }
+    
+    if ((connect(sockfd, res, sizeof(res))) == -1) {
+        perror("connect");
+        return 1;
+    }
+
+    return 0;
+}
